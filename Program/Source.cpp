7132013d49b41e06bcd87d4d935a590bfbe24bdf
petykokusz79 <petykokusz.79@gmail.com>
2021-06-25 17:53:48 +0200

    változótípusok kezelése, fájlművelet-optimalizáció, esztétikai csiszolás (régebbi, javítandó koncepció)

--- Program/Source.cpp
+++ Program/Source.cpp
@@ -6,58 +6,74 @@
 
 using namespace std;
 
-const int routes_size = 190000000;
-const int HEAP_size = 10000000;
-const int nodes = 87;
+using Tio = int; // type of input and output elements
+using Tn = unsigned char; // type of nodes
+using Tl = unsigned short; // type of lengths
+using Ti = int; // type of route indexes
+using Th = int; // type of heap indexes
+
+const Tl Tl_MAX = USHRT_MAX; // maximum value of lengths
+const char Tn_SIZE = sizeof(Tn); // size of nodes
+const char Ti_SIZE = sizeof(Ti); // size of route indexes
+const char Tf_SIZE = Tn_SIZE + Ti_SIZE; // size of file elements
+
+const Th HEAP_size = 240000000;
+const Tn nodes = 87;
 
 struct edge {
-	unsigned char end;
-	unsigned short weight;
+	Tn end;
+	Tl weight;
 };
 
 ofstream o("output.txt");
-vector<unsigned short> distance_from_startS(nodes + 1, USHRT_MAX);
+vector<Tl> distance_from_startS(nodes + 1, Tl_MAX);
 
 fstream r("routes.txt", ios::binary | ios::in | ios::out | ios::trunc);
-int routes_count = 0;
-void write(pair<unsigned char, int> route) { // last, index of previous
-	r.seekp(routes_count * sizeof(pair<unsigned char, int>));
-	r.write((char*)&route, sizeof(pair<unsigned char, int>));
+Ti routes_count = 0;
+void write(pair<Tn, Ti> route) { // last, index of previous
+	r.seekp(routes_count * Tf_SIZE);
+	r.write((char*)&route.first, Tn_SIZE);
+	r.write((char*)&route.second, Ti_SIZE);
 }
-pair<unsigned char, int> read(int index) { // last, index of previous
-	pair<unsigned char, int> result;
-	r.seekg(index * sizeof(pair<unsigned char, int>));
-	r.read((char*)&result, sizeof(pair<unsigned char, int>));
+void read(pair<Tn, Ti> &result, Ti index) { // last, index of previous
+	r.seekg(index * Tf_SIZE);
+	r.read((char*)&result.first, Tn_SIZE);
+	r.read((char*)&result.second, Ti_SIZE);
+}
+Tn read_node(Ti index) {
+	Tn result;
+	r.seekg(index * Tf_SIZE);
+	r.read((char*)&result, Tn_SIZE);
 	return result;
 }
 
-int HEAP_count = 0;
-pair<int, int> HEAP_routeS[HEAP_size + 1]; // length, index in "r"
-void HEAP_push(unsigned short length, unsigned char last, int previous) {
-	if (HEAP_count == HEAP_size || routes_count == routes_size) {
+Th HEAP_count = 0;
+pair<Tl, Ti> HEAP_routeS[HEAP_size + 1]; // length, index in "r"
+void HEAP_push(Tl length, Tn last, Ti previous) {
+	if (HEAP_count == HEAP_size/* || routes_count == routes_size*/) {
 		cout << "Megtelt. " << HEAP_count << " " << routes_count << '\n';
 		o << endl;
 		exit(0);
 	}
 	length += distance_from_startS[last]; // prioritás legrövidebb körre vonatkozó alsó becslés alapján
-	int index = ++HEAP_count; // index a végén
+	Th index = ++HEAP_count; // index a végén
 	while (index != 1 && HEAP_routeS[index / 2].first > length) {
 		HEAP_routeS[index] = HEAP_routeS[index / 2]; // fentebbi süllyesztése
 		index /= 2;
 	}
 	HEAP_routeS[index] = { length, ++routes_count }; // új elem helyreillesztése
-	write({ last, previous }); // új elem fájlba írása rakása
+	write({ last, previous }); // új elem fájlba írása
 }
-void HEAP_pop(pair<unsigned short, int> &result) { // length, index in "r"
-	int index = HEAP_routeS[1].second;
-	result = { HEAP_routeS[1].first - distance_from_startS[read(index).first], index }; // eredmény mentése a prioritási becslés kivonásával
+void HEAP_pop(pair<Tl, Ti> &result) { // length, index in "r"
+	Ti place = HEAP_routeS[1].second;
+	result = { HEAP_routeS[1].first - distance_from_startS[read_node(place)], place }; // eredmény mentése a prioritási becslés kivonásával
 	--HEAP_count;
 	if (HEAP_count == 0) {
 		return;
 	}
-	index = 1; // index az elején
+	Th index = 1; // index az elején
 	while (index * 2 <= HEAP_count) {
-		int shorter_child = index * 2 == HEAP_count ? 0 : HEAP_routeS[index * 2 + 1].first < HEAP_routeS[index * 2].first ? 1 : 0; // rövidebb gyerek
+		bool shorter_child = index * 2 == HEAP_count ? 0 : HEAP_routeS[index * 2 + 1].first < HEAP_routeS[index * 2].first ? 1 : 0; // rövidebb gyerek
 		if (HEAP_routeS[index * 2 + shorter_child].first >= HEAP_routeS[HEAP_count + 1].first) { // nem süllyeszhetõ
 			break;
 		}
@@ -67,32 +83,32 @@ void HEAP_pop(pair<unsigned short, int> &result) { // length, index in "r"
 	HEAP_routeS[index] = HEAP_routeS[HEAP_count + 1]; // utolsó elem helyreillesztése
 }
 
-int edges;
+Tio edges;
 vector<vector<edge>> edgeS(nodes + 1);
-int HEAP_max = 0;
+Th HEAP_max = 0;
 
 void generate_edges() {
 	// --- Beolvasott éllistából szomszédsági mátrix --- //
-	vector<vector<unsigned short>> edge_betweenS(nodes + 1, vector<unsigned short>(nodes + 1));
-	for (int i = 0; i < edges; i++) {
-		int s, e, w;
+	vector<vector<Tl>> edge_betweenS(nodes + 1, vector<Tl>(nodes + 1));
+	for (Tio i = 0; i < edges; i++) {
+		Tio s, e, w;
 		cin >> s >> e >> w;
-		edge_betweenS[s][e] = edge_betweenS[e][s] = (unsigned short)w;
+		edge_betweenS[(Tn)s][(Tn)e] = edge_betweenS[(Tn)e][(Tn)s] = (Tl)w;
 	}
 	// --- Zsákélek törlése --- //
-	for (int i = 1; i <= nodes; i++) {
-		for (int j = i + 1; j <= nodes; j++) {
+	for (Tn i = 1; i <= nodes; i++) {
+		for (Tn j = i + 1; j <= nodes; j++) {
 			if (edge_betweenS[i][j] != 0) {
 				// --- Kiválasztott él törlése --- //
-				int length = edge_betweenS[i][j];
+				Tl length = edge_betweenS[i][j];
 				edge_betweenS[i][j] = edge_betweenS[j][i] = 0;
 				// --- Az 1-es csúcsot tartalmazó komponens szélességi bejárása --- //
 				vector<bool> visitedS(nodes + 1);
-				queue<unsigned char> currentS;
+				queue<Tn> currentS;
 				currentS.push(1);
 				visitedS[1] = true;
 				while (!currentS.empty()) {
-					for (int k = 1; k <= nodes; k++) {
+					for (Tn k = 1; k <= nodes; k++) {
 						if (edge_betweenS[currentS.front()][k] != 0 && !visitedS[k]) {
 							currentS.push(k);
 							visitedS[k] = true;
@@ -103,9 +119,9 @@ void generate_edges() {
 				// --- Kiválasztott él visszaírása --- //
 				edge_betweenS[i][j] = edge_betweenS[j][i] = length;
 				// --- Az elõzõleg nem bejárt csúcsokat érintõ élek törlése --- //
-				for (int k = 1; k <= nodes; k++) {
+				for (Tn k = 1; k <= nodes; k++) {
 					if (!visitedS[k]) {
-						for (int l = 1; l <= nodes; l++) {
+						for (Tn l = 1; l <= nodes; l++) {
 							edge_betweenS[k][l] = edge_betweenS[l][k] = 0;
 						}
 					}
@@ -114,11 +130,11 @@ void generate_edges() {
 		}
 	}
 	// --- Szomszédsági lista felépítése a szomszédsági mátrix alapján --- //
-	for (int i = 1; i <= nodes; i++) {
-		for (int j = i; j <= nodes; j++) {
+	for (Tn i = 1; i <= nodes; i++) {
+		for (Tn j = i; j <= nodes; j++) {
 			if (edge_betweenS[i][j] != 0) {
-				edgeS[i].push_back({ (unsigned char)j, edge_betweenS[i][j] });
-				edgeS[j].push_back({ (unsigned char)i, edge_betweenS[i][j] });
+				edgeS[i].push_back({ j, edge_betweenS[i][j] });
+				edgeS[j].push_back({ i, edge_betweenS[i][j] });
 			}
 		}
 	}
@@ -130,13 +146,13 @@ void generate_edges() {
 	}
 }
 
-void dijkstra(int n) {
+void dijkstra(Tn n) {
 	distance_from_startS[n] = 0;
-	priority_queue<pair<unsigned short, unsigned char>, vector<pair<unsigned short, unsigned char>>, greater<pair<unsigned short, unsigned char>>> currentS;
+	priority_queue<pair<Tl, Tn>, vector<pair<Tl, Tn>>, greater<pair<Tl, Tn>>> currentS;
 	currentS.push({ 0, n });
 	while (!currentS.empty()) {
-		unsigned short length = currentS.top().first;
-		unsigned char node = currentS.top().second;
+		Tl length = currentS.top().first;
+		Tn node = currentS.top().second;
 		currentS.pop();
 		if (length != distance_from_startS[node]) { // már van rövidebb út a csúcsba
 			continue;
@@ -150,23 +166,28 @@ void dijkstra(int n) {
 	}
 }
 
-void write_route(int index) {
+void write_route(Ti index) {
+	pair<Tn, Ti> route;
+	read(route, index);
 	if (index != 0) {
-		write_route(read(index).second);
+		write_route(route.second);
 	}
-	o << (int)read(index).first << " ";
+	o << (Tio)route.first << " ";
 }
 
 void find_routes() {
 	int answers = 0;
 	write({ 1, 0 });
-	pair<unsigned short, int> c;
+	pair<Tl, Ti> c;
+	pair<Tn, Ti> route;
 	HEAP_push(edgeS[1][0].weight, 2, 0);
 	while (HEAP_count > 0) {
 		// --- Útvonal kivétele a prioritási sorból --- //
 		HEAP_pop(c);
+		// --- Útvonal végének kiolvasása a fájlból --- //
+		read(route, c.second);
 		// --- Megtalált útvonal fájlba írása --- //
-		if (read(c.second).first == 1) {
+		if (route.first == 1) {
 			HEAP_max = max(HEAP_max, HEAP_count);
 			cout << ++answers << " " << c.first << " " << HEAP_count << " " << routes_count << '\n';
 			o << c.first << '\n';
@@ -176,15 +197,16 @@ void find_routes() {
 		}
 		// --- Kivett útvonal érintett éleinek mátrixba írása --- //
 		bool visitedS[nodes + 1][nodes + 1] = {};
-		unsigned char last = read(c.second).first;
-		int previous = read(c.second).second;
+		Tn last = route.first;
+		Ti previous = route.second;
 		while (last != 1) {
-			visitedS[last][read(previous).first] = visitedS[read(previous).first][last] = true;
-			last = read(previous).first;
-			previous = read(previous).second;
+			read(route, previous);
+			visitedS[last][route.first] = visitedS[route.first][last] = true;
+			last = route.first;
+			previous = route.second;
 		}
 		// --- Kivett útvonal utolsó csúcsából továbbinduló utak prioritási sorba rakása --- //
-		last = read(c.second).first;
+		last = read_node(c.second);
 		for (auto &e : edgeS[last]) {
 			if (!visitedS[last][e.end]) {
 				// --- 1-es csúcsba vezetõ szakasz prioritási sorba rakása --- //
@@ -194,16 +216,16 @@ void find_routes() {
 				}
 				// --- Szélességi bejárás annak vizsgálatára, hogy a továbbinduló út körbeérhet-e --- //
 				visitedS[last][e.end] = visitedS[e.end][last] = true;
-				unsigned char bfs_visitedS[nodes + 1] = {};
-				queue<unsigned char> from_firstS;
+				Tn bfs_visitedS[nodes + 1] = {};
+				queue<Tn> from_firstS;
 				from_firstS.push(1);
 				bfs_visitedS[1] = 1;
-				queue<unsigned char> from_lastS;
+				queue<Tn> from_lastS;
 				from_lastS.push(e.end);
 				bfs_visitedS[e.end] = 2;
 				bool circle = false;
 				while (!from_firstS.empty() && !from_lastS.empty()) {
-					unsigned char bfs_c = from_firstS.front();
+					Tn bfs_c = from_firstS.front();
 					for (auto f : edgeS[bfs_c]) {
 						if (bfs_visitedS[f.end] == 2) {
 							circle = true;
